07bd012cc1bec7c97d7199b6f646983e6644efd8
Ilya Zhuravlev <whatever@xyz.is>
2021-01-14 15:21:47 -0500

    add xyz keyboards

--- bootloader/src/config.h
+++ bootloader/src/config.h
@@ -20,6 +20,20 @@
 #define BL_OUTPUT_PIN 12
 #define BL_INPUT_BANK GPIOB
 #define BL_INPUT_PIN 13
+#elif defined(TARGET_XYZ60)
+#define VIAL_KEYBOARD_UID {0x36, 0xCD, 0x23, 0xE1, 0xB4, 0x0F, 0xC3, 0xE4}
+// col6, row4
+#define BL_OUTPUT_BANK GPIOB
+#define BL_OUTPUT_PIN 0
+#define BL_INPUT_BANK GPIOB
+#define BL_INPUT_PIN 15
+#elif defined(TARGET_XYZ8X)
+#define VIAL_KEYBOARD_UID {0x7F, 0x5E, 0x10, 0x51, 0x72, 0xE0, 0xF5, 0x88}
+// col3, row10
+#define BL_OUTPUT_BANK GPIOB
+#define BL_OUTPUT_PIN 15
+#define BL_INPUT_BANK GPIOA
+#define BL_INPUT_PIN 2
 #else
 #error Unknown target
 #endif
